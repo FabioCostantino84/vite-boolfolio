e2b72463a68522130bd894168b995dbc23783c83
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-17 17:41:37 +0100

    add call ajax

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,45 @@ export default {
   data() {
     return {
 
+      base_url: 'http://127.0.0.1:8000/',
+      projects_api: 'api/projects',
+
+      projects: [],
+
     }
   },
   mounted() {
 
+    axios
+
+      .get(this.projects_api)
+      .then(response => {
+        console.log(response);
+        this.projects = response.data.result;
+      })
+
+      .catch(err => {
+        console.error(err);
+      });
+
   }
 }
+
 </script>
 
 <template>
   <div id="app">
 
-    <h1>Ciao</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div v-for="project in projects.data" class="card text-center">
+            <h2 >{{ project.title }}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
