9db87ec9a9faa2aea0cd4af49658b52cc0b24ac0
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-20 15:23:20 +0100

    add route.js and import use

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+//import dal fil router.js l'export {router}
+import { router } from './router.js';
+//importo il router con lo use()
+createApp(App).use(router).mount('#app');
